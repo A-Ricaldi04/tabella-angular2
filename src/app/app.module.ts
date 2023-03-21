0578770b10a3a19a91a082716ae3f00bce940a88
lai2-01 <lai2-01@itismeucci.com>
2023-03-21 07:43:23 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabellaComponent } from './tabella/tabella.component';
+import {MatTableModule} from '@angular/material/table';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabellaComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatTableModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
